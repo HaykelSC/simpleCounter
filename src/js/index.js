5d9819924a8c341ce3578dc38f3c157ea46d29b4
HaykelSC <haykel.sibaja@gmail.com>
2021-03-25 06:55:59 +0000

    new index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,43 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-//include bootstrap npm library into the bundle
-import "bootstrap";
-
-//include your index.scss file into the bundle
 import "../styles/index.scss";
+import propTypes from "prop-types";
 
-//import your own components
-import { Home } from "./component/home.js";
+//create your first component
+function SimpleCounter(props) {
+	return (
+		<div className="counter">
+			<div className="Clock">
+				<i className="fas fa-stopwatch"></i>
+			</div>
+			<div className="four">{props.digitFour % 10}</div>
+			<div className="three">{props.digitThree % 10}</div>
+			<div className="two">{props.digitTwo % 10}</div>
+			<div className="one">{props.digitOne % 10}</div>
+		</div>
+	);
+}
+SimpleCounter.propTypes = {
+	digitFour: propTypes.number,
+	digitThree: propTypes.number,
+	digitTwo: propTypes.number,
+	digitOne: propTypes.number
+};
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function() {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	counter++;
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
